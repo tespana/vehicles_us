e30a6cbb4ed561997b7e5869c61dedf6da089e12
Tania <taniage96@hotmail.com>
2024-06-23 13:08:25 -0600

    ok

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if Scatter_button: # al hacer clic en el botón
     st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
             
             # crear un histograma
-    fig2 = px.scatter(car_data, x="odometer")
+    fig2 = px.scatter(car_data, x="odometer",y="price")
         
             # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig2, use_container_width=True)
